9fa83f3c85060135b577a570b3b16c281143e050
wagadao.y <wagadao.y@outlook.com>
2026-04-26 23:30:02 +0900

    renderer-threeへ描画ヘルパーを移動

--- packages/core/src/renderer-three/index.ts
+++ packages/core/src/renderer-three/index.ts
@@ -2,5 +2,6 @@ export * from "./features";
 export * from "./geometry/point-cloud-octree-node";
 export * from "./math/box3-like";
 export * from "./picking/point-cloud-octree-picker";
+export * from "./rendering/potree-renderer";
 export * from "./scene/point-cloud-tree";
 export * from "./types";
